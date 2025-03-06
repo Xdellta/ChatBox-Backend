b84e787b42af5a34678c0d37cc24899ccd9b217b
Patryk Piotrowski <xdelllta@gmail.com>
2025-03-06 18:36:05 +0100

    ChatBox-Backend: setup

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const protocol = process.env.PROTOCOL || 'http';
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at ${protocol}://${host}:${port}`);
+});
