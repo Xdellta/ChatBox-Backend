d6a66808216fa9b5004bc85dc535f3c786c62630
Patryk Piotrowski <xdelllta@gmail.com>
2025-03-27 13:10:45 +0100

    Check if the token is blacklisted

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,6 @@ const jwt = require('jsonwebtoken');
 async function login(req, res, next) {
   const { email, password } = req.body;
 
-  // Validate inputs
   if (!email || !password) {
     return next(new HttpError(400, 'Email and password are required.', null, req));
   }
@@ -55,7 +54,6 @@ async function login(req, res, next) {
 async function register(req, res, next) {
   const { username, email, password } = req.body;
 
-  // Validate inputs
   if (!username || !email || !password) {
     return next(new HttpError(400, 'Username, email and password are required.', null, req));
   }
@@ -73,11 +71,14 @@ async function register(req, res, next) {
   }
 
   try {
+    // Check if user already exists
     const existingUser = await prisma.user.findUnique({ where: { email } });
+
     if (existingUser) {
       return next(new HttpError(400, 'A user with this email already exists.', null, req));
     }
 
+    // Hash password and create new user
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = await prisma.user.create({
@@ -123,16 +124,28 @@ async function refreshToken(req, res, next) {
   }
 
   try {
-    // Sprawdzić w bazie danych czy refresh token jest na czarnej liście
+    // Check if the token is blacklisted
+    const blacklistedToken = await prisma.jwtBlacklist.findUnique({
+      where: {
+        token: refreshToken,
+      },
+    });
 
+    if (blacklistedToken) {
+      return next(new HttpError(401, 'Unauthorized.', 'Token is blacklisted.', req));
+    }
+
+    // Verify the refresh token
     const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
 
+    // Generate new access token
     const accessToken = jwt.sign({ userId: decoded.userId }, process.env.JWT_ACCESS_SECRET, {
       expiresIn: process.env.ACCESS_EXPIRATION,
     });
 
     res.setHeader('Authorization', `Bearer ${accessToken}`);
     res.status(200).json({ message: 'Token refreshed.' });
+
   } catch (error) {
     if (error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') {
       return next(new HttpError(401, 'Unauthorized.', error.message, req));
@@ -141,5 +154,6 @@ async function refreshToken(req, res, next) {
     return next(new HttpError(500, 'An error occurred while processing the refresh token request.', error.message, req));
   }
 }
+
 
 module.exports = { login, register, refreshToken };
